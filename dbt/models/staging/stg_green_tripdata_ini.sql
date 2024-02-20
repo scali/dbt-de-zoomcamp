5139f42b35fc1a85f3435a852ff864d1c4b645e2
Marc Lherbette <mlherbette@biotope.fr>
2024-02-20 22:47:33 +0000

    workshop + homework

---
+++ dbt/models/staging/stg_green_tripdata_ini.sql
@@ -0,0 +1,43 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'green_tripdata') }}
+
+),
+
+renamed as (
+
+    select
+        {{ dbt_utils.generate_surrogate_key(['vendor_id', 'pickup_datetime']) }} as trip_id,
+        vendor_id,
+        pickup_datetime,
+        dropoff_datetime,
+        store_and_fwd_flag,
+        rate_code,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        airport_fee,
+        total_amount,
+        payment_type,
+        {{ get_payment_type_description('payment_type') }} as payment_type_descripted,
+        distance_between_service,
+        time_between_service,
+        trip_type,
+        imp_surcharge,
+        pickup_location_id,
+        dropoff_location_id,
+        data_file_year,
+        data_file_month
+
+    from source
+
+)
+
+select * from renamed
